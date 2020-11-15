1e1769ed11373312b9105329381abf934b9e03f2
vahid <vahid.borhani@gmail.com>
2020-11-15 18:07:30 +0330

    add prettierrc.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from "./map";
-export * from "./make-it";
-export * from "./maker";
+export * from './map';
+export * from './make-it';
+export * from './maker';
--- src/make-it.ts
+++ src/make-it.ts
@@ -1,35 +1,31 @@
 /* eslint-disable prefer-rest-params */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import "reflect-metadata";
-import { Exception, ClassType, ArgumentsCounter } from "basecript";
-import { Maker } from "./maker";
-import { IMap } from "./map";
+import 'reflect-metadata';
+import { Exception, ClassType, ArgumentsCounter } from 'basecript';
+import { Maker } from './maker';
+import { IMap } from './map';
 
-export const MakeItKey = "MAKE-IT";
+export const MakeItKey = 'MAKE-IT';
 
 export function mapping(map: IMap) {
-  return (target: unknown, key: string | symbol): void => {
-    if (ArgumentsCounter(arguments, undefined) > 2) {
-      throw new Exception(`Decorator '${MakeItKey}' is for properties only.`);
-    }
-    const constructor = (target as any).constructor;
-    let maker: Maker | undefined = Reflect.getMetadata(MakeItKey, constructor);
-    if (maker === undefined) maker = new Maker();
-    if (maker.has(key)) {
-      throw new Exception(
-        `Decorator '${MakeItKey}' can only be applied once to '${key.toString}'`
-      );
-    } else {
-      maker.set(key, map);
-      Reflect.defineMetadata(MakeItKey, maker, constructor);
-    }
-  };
+    return (target: unknown, key: string | symbol): void => {
+        if (ArgumentsCounter(arguments, undefined) > 2) {
+            throw new Exception(`Decorator '${MakeItKey}' is for properties only.`);
+        }
+        const constructor = (target as any).constructor;
+        let maker: Maker | undefined = Reflect.getMetadata(MakeItKey, constructor);
+        if (maker === undefined) maker = new Maker();
+        if (maker.has(key)) {
+            throw new Exception(`Decorator '${MakeItKey}' can only be applied once to '${key.toString}'`);
+        } else {
+            maker.set(key, map);
+            Reflect.defineMetadata(MakeItKey, maker, constructor);
+        }
+    };
 }
 
 export function makeIt<T>(constructor?: ClassType<T>, pool?: unknown): T {
-  let maker: Maker | undefined = pool
-    ? Reflect.getMetadata(MakeItKey, (pool as any).constructor)
-    : undefined;
-  if (maker === undefined) maker = new Maker();
-  return maker.make(constructor, pool);
+    let maker: Maker | undefined = pool ? Reflect.getMetadata(MakeItKey, (pool as any).constructor) : undefined;
+    if (maker === undefined) maker = new Maker();
+    return maker.make(constructor, pool);
 }
